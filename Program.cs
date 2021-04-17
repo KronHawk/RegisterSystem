7aca60e25c574e54a4071d9e38399a5530d4f889
KronHawk <josevf002@gmail.com>
2021-04-17 17:45:38 -0300

    System of Register or longin, based on the lest project

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace RegisterBasic
+{
+    class Program
+    {
+        static string Info()
+        {
+            Console.WriteLine("write anything:");
+            string text = Console.ReadLine();
+            return text;
+        }
+        static string Register()
+        {
+            Console.WriteLine("Register or Login?");
+            Console.WriteLine("1- Register");
+            Console.WriteLine("2- Login");
+            Console.WriteLine("X- Leave");
+
+            string choose = Console.ReadLine();
+            return choose;
+        }
+        static void Main(string[] args)
+        {
+            Database[] user = new Database[50];
+            string choose = Register();
+            while (choose.ToUpper() != "X")
+            {
+                switch (choose)
+                {
+                    case "1":
+
+                        Database use;
+                        Console.WriteLine("Choose your Username");
+                        string Name = Console.ReadLine();
+                        use.UserName = Name;
+
+                        Console.Clear();
+
+                        Console.WriteLine("Your password:");
+                        string password = Console.ReadLine();
+                        use.Password = password;
+                        Console.Clear();
+
+                        break;
+                    case "2":
+                        //building the simple system
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                choose = Register();
+            }
+        }
+    }
+}
