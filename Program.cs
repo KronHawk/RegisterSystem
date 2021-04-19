f088a5b75268c9262181175ab3e64166d9c8129b
KronHawk <josevf002@gmail.com>
2021-04-19 16:51:45 -0300

    Login System implemented

--- Program.cs
+++ Program.cs
@@ -12,10 +12,12 @@ namespace RegisterBasic
         }
         static string Register()
         {
+            Console.WriteLine();
             Console.WriteLine("Register or Login?");
             Console.WriteLine("1- Register");
             Console.WriteLine("2- Login");
             Console.WriteLine("X- Leave");
+            Console.WriteLine();
 
             string choose = Console.ReadLine();
             return choose;
@@ -24,27 +26,59 @@ namespace RegisterBasic
         {
             Database[] user = new Database[50];
             string choose = Register();
+            var Allusers = 0;
             while (choose.ToUpper() != "X")
             {
                 switch (choose)
                 {
                     case "1":
 
-                        Database use;
+                        Database users = new Database();
                         Console.WriteLine("Choose your Username");
-                        string Name = Console.ReadLine();
-                        use.UserName = Name;
+                        string Name = Console.ReadLine().ToUpper();
+                        users.UserName = Name;
 
                         Console.Clear();
 
                         Console.WriteLine("Your password:");
-                        string password = Console.ReadLine();
-                        use.Password = password;
+                        string password = Console.ReadLine().ToUpper();
+                        users.Password = password;
                         Console.Clear();
+
+                        user[Allusers] = users;
+                        Allusers++;
 
                         break;
                     case "2":
-                        //building the simple system
+                        bool verif = false, verifPass = false;
+                        string verifUser, verifPW;
+                        Console.WriteLine("Your Username:");
+                        string loginUser = Console.ReadLine().ToUpper();
+                        foreach (var a in user)
+                        {
+                            verifUser = a.UserName;
+                            if (loginUser == verifUser)
+                            {
+                                verif = true;
+                            }
+                        }
+                        Console.Clear();
+                        Console.WriteLine("Your Password:");
+                        string loginPassword = Console.ReadLine().ToUpper();
+                        foreach (var b in user)
+                        {
+                            verifPW = b.Password;
+                            if (loginPassword == verifPW)
+                            {
+                                verifPass = true;
+                            }
+                        }
+                        if (verif == true && verifPass == true)
+                        {
+                            //logado
+                            Console.WriteLine("logado");
+                        }
+                        break;
                     default:
                         throw new ArgumentOutOfRangeException();
                 }
